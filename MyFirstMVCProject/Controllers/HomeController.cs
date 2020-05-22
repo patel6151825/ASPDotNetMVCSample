8007194a828039a1259390774db45c357044cd65
patel6151825 <6151825@johnabbottcollege.net>
2020-05-21 20:15:37 -0400

    Initila upload of MyFirstMVCSampleProject with some changes in files

---
+++ MyFirstMVCProject/Controllers/HomeController.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MyFirstMVCProject.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+
+        public string HelloWorld(string firstName,string lastName)
+        {
+            return $"Hello World from {firstName} {lastName} !!!";
+        }
+
+        public ActionResult GetUser(int id)
+        {
+            IList<int> myList = new List<int>() { 1,2,3,4};
+            myList.Add(5);
+
+            ViewBag.Message = $"Hello from the User_{id}";
+            ViewBag.MyList = myList;
+            return View(); ;
+        }
+    }
+}

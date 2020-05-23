2db26eaa6b11a2f3fa2e26baf01d840560ef1752
patel6151825 <6151825@johnabbottcollege.net>
2020-05-22 20:28:34 -0400

    Adding the Controller, Model and Views for Student

---
+++ MyFirstMVCProject/Controllers/StudentController.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MyFirstMVCProject.Controllers
+{
+    public class StudentController : Controller
+    {
+        // GET: Student
+        public ActionResult Index()
+        {
+            
+            //MvcApplication.studentsList.Add(new Models.Student { StudentId = 1, StudentName = "Payal", Age = 23});
+            //MvcApplication.studentsList.Add(new Models.Student { StudentId = 2, StudentName = "Patel", Age = 33 });
+
+            return View(MvcApplication.studentsList);
+        }
+
+        // GET: Student/Details/5
+        public ActionResult Details(int id)
+        {
+            return View();
+        }
+
+        // GET: Student/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Student/Create
+        [HttpPost]
+        public ActionResult Create(MyFirstMVCProject.Models.Student student)
+        {
+            try
+            {
+                // TODO: Add insert logic here
+                student.StudentId = ++MvcApplication.globalStudentId;
+                MvcApplication.studentsList.Add(student);
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Student/Edit/5
+        public ActionResult Edit(int id)
+        {
+            return View();
+        }
+
+        // POST: Student/Edit/5
+        [HttpPost]
+        public ActionResult Edit(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add update logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Student/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        // POST: Student/Delete/5
+        [HttpPost]
+        public ActionResult Delete(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add delete logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
---
+++ MyFirstMVCProject/Models/Student.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace MyFirstMVCProject.Models
+{
+    public class Student
+    {
+        public int StudentId { get; set; }
+
+        [Required]
+        public string StudentName { get; set; }
+
+        [Range(5, 50)]
+        public int Age { get; set; }
+
+
+    }
+}
